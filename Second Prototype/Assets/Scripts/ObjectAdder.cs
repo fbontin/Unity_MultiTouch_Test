793b4d22dd00a6f051bb49c3fe718c0ad07f3201
Filip Bontin <fbontin@gmail.com>
2017-07-25 14:29:39 +0200

    Drag to add object first implementation

--- Second Prototype/Assets/Scripts/ObjectAdder.cs
+++ Second Prototype/Assets/Scripts/ObjectAdder.cs
@@ -1,6 +1,9 @@
-﻿using TouchScript.Behaviors;
+﻿using TouchScript;
+using TouchScript.Behaviors;
+using TouchScript.Gestures;
 using TouchScript.Gestures.TransformGestures;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
@@ -9,37 +12,106 @@ namespace Assets.Scripts
 
 		public void AddObject(string objectType)
 		{
-			PrimitiveType primitiveType;
-
-			switch (objectType)
-			{
-				case "Cube":
-					primitiveType = PrimitiveType.Cube;
-					break;
-				case "Sphere":
-					primitiveType = PrimitiveType.Sphere;
-					break;
-				default:
-					primitiveType = PrimitiveType.Capsule;
-					break;
-			}
-
+			var primitiveType = ParsePrimitiveType(objectType);
 			Debug.Log("Adding " + objectType);
 
 			var plane = GameObject.Find("Plane");
-			var cube = GameObject.CreatePrimitive(primitiveType);
-			//set plane as parent
-			cube.transform.parent = plane.transform;
+			var go = GameObject.CreatePrimitive(primitiveType);
+			go.transform.parent = plane.transform;
 
-			cube.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
-			cube.transform.localPosition = new Vector3(1f, 0.5f, 1f);
+			go.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+			go.transform.localPosition = new Vector3(1f, 0.5f, 1f);
 
-			cube.AddComponent<StayOnGround>();
-			cube.AddComponent<TransformGesture>();
-			var tg = cube.GetComponent<TransformGesture>();
+			AddComponentsToGameObject(go);
+		}
+
+		public void AddObjectWithCoordinates(string objectType)
+		{
+			var primitiveType = ParsePrimitiveType(objectType);
+
+			var plane = GameObject.Find("Plane");
+			var go = GameObject.CreatePrimitive(primitiveType);
+			go.transform.parent = plane.transform;
+
+			go.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+
+			SetPosition(go, plane);
+			AddComponentsToGameObject(go);
+			ChangeTouchFocus();
+		}
+
+		private static void ChangeTouchFocus()
+		{
+			//disable scrollview which includes the buttons
+			var scrollView = GameObject.Find("Scroll View");
+			//var sv = scrollView.GetComponent<ScrollRect>();
+			
+
+			scrollView.SetActive(false);
+			
+			//var button = GameObject.Find("Cube Button").GetComponent<Button>();
+			//button.interactable = false;
+
+			//cancel pointer/touch
+			var tm = TouchManager.Instance;
+			var firstPointerId = tm.PressedPointers[0].Id;
+			tm.CancelPointer(firstPointerId, true);
+		}
+
+		private static void SetPosition(GameObject go, GameObject plane)
+		{
+			var tapPosition = FindTapPosition();
+
+			var cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+			var distance = Vector3.Distance(cam.transform.position, plane.transform.position);
+			var ray = cam.ScreenPointToRay(new Vector3(tapPosition.x, tapPosition.y, distance));
+
+			var pl = new Plane(Vector3.up, Vector3.zero);
+			float hitDistance;
+			pl.Raycast(ray, out hitDistance);
+
+			go.transform.position = ray.origin + hitDistance * ray.direction;
+		}
+
+		private static Vector2 FindTapPosition()
+		{
+			var tm = TouchManager.Instance;
+			if (tm.PressedPointersCount > 0)
+			{
+				var pos = tm.PressedPointers[0].Position;
+				Debug.Log("Real position: " + pos);
+				return pos;
+			}
+			Debug.Log("Position (0, 0) used");
+			return new Vector2(0, 0);
+		}
+
+		private static PrimitiveType ParsePrimitiveType(string objectType)
+		{
+			objectType = objectType.ToLower();
+			switch (objectType)
+			{
+				case "cube":
+					return PrimitiveType.Cube;
+				case "sphere":
+					return PrimitiveType.Sphere;
+				default:
+					return PrimitiveType.Capsule;
+			}
+		}
+
+		private static void AddComponentsToGameObject(GameObject go)
+		{
+			go.AddComponent<StayOnGround>();
+			go.AddComponent<TransformGesture>();
+
+			go.AddComponent<TapGesture>();
+			go.AddComponent<TappedHandler>();
+
+			var tg = go.GetComponent<TransformGesture>();
 			tg.Projection = TransformGesture.ProjectionType.Global;
 			tg.ProjectionPlaneNormal = new Vector3(0, 1, 0);
-			cube.AddComponent<Transformer>();
+			go.AddComponent<Transformer>();
 		}
 	}
 }
--- Second Prototype/Assets/Scripts/TappedHandler.cs
+++ Second Prototype/Assets/Scripts/TappedHandler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TouchScript.Gestures;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,45 +10,96 @@ namespace Assets.Scripts
 {
 	public class TappedHandler : MonoBehaviour
 	{
-
-		public GameObject PrefabButton;
+	
+		private readonly Vector3 _menuOffset = new Vector3(50, 50);
 
 		// Use this for initialization
 		void Start ()
 		{
 			var tg = gameObject.GetComponent<TapGesture>();
 			tg.UseUnityEvents = true;
-			tg.Tapped += ChangeColor;
 			tg.Tapped += ShowActionMenu;
 		}
 
-		private void ChangeColor(object sender, EventArgs e)
+		private void ShowActionMenu(object sender, EventArgs e)
+		{
+			//get scene objects
+			var cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+			var screenPosition = cam.WorldToScreenPoint(gameObject.transform.position);
+			var canvas = GameObject.Find("UI Canvas").GetComponent<Canvas>();
+			var buttons = new List<Button>();
+
+			//create background button first (as it should be behind the other buttons)
+			CreateBackgroundButton(canvas, buttons);
+			CreateRemoveButton(canvas, screenPosition, buttons);
+			CreateNewColorButton(canvas, screenPosition, buttons);
+			CreateDuplicateButton(canvas, screenPosition, buttons);
+		}
+
+		private void CreateDuplicateButton(Canvas canvas, Vector3 screenPosition, List<Button> buttons)
+		{
+			var copyButton = CreateButton(canvas, buttons, "Duplicate");
+			copyButton.transform.position = screenPosition + _menuOffset + new Vector3(0, 80);
+			copyButton.onClick.AddListener(DuplicateGameObject);
+			copyButton.onClick.AddListener(() => DestroyActionMenu(buttons));
+		}
+
+		private void DuplicateGameObject()
+		{
+			var copy = Instantiate(gameObject);
+			copy.transform.parent = gameObject.transform.parent;
+			copy.transform.localScale = gameObject.transform.localScale;
+
+			var oldPos = gameObject.transform.localPosition;
+			var oldScale = gameObject.transform.localScale;
+			var newPos = new Vector3(oldScale.x, 0, oldScale.z) / -2;
+			copy.transform.localPosition = oldPos + newPos;
+		}
+
+		private void CreateNewColorButton(Canvas canvas, Vector3 screenPosition, List<Button> buttons)
+		{
+			var newColorButton = CreateButton(canvas, buttons, "New Color");
+			newColorButton.transform.position = screenPosition + _menuOffset + new Vector3(0, 40);
+			newColorButton.onClick.AddListener(ChangeColor);
+		}
+
+		private void ChangeColor()
 		{
 			var rend = gameObject.GetComponent<Renderer>();
 			rend.material.SetColor("_Color", new Color(Random.value, Random.value, Random.value));
 		}
 
-		private void ShowActionMenu(object sender, EventArgs e)
+		private void CreateRemoveButton(Canvas canvas, Vector3 screenPosition, List<Button> buttons)
 		{
-			var cam = GameObject.Find("Main Camera").GetComponent<Camera>();
-			var screenPosition = cam.WorldToScreenPoint(gameObject.transform.position);
-			Debug.Log("Screen pos: " + screenPosition.x + ", " + screenPosition.y);
-
-			var canvas = GameObject.Find("UI Canvas").GetComponent<Canvas>();
-
-			var button = Instantiate(PrefabButton);
-			button.transform.SetParent(canvas.transform);
-			button.transform.position = screenPosition + new Vector3(50, 50);
-
-			var text = button.GetComponentInChildren<Text>();
-			text.text = "Remove";
-
-			//TODO: Connect button to cube somehow. Then make the button remove/destroy the cube.
+			var removeButton = CreateButton(canvas, buttons, "Remove");
+			removeButton.transform.position = screenPosition + _menuOffset;
+			removeButton.onClick.AddListener(() => DestroyActionMenu(buttons));
+			removeButton.onClick.AddListener(() => Destroy(gameObject));
 		}
 
-		// Update is called once per frame
-		void Update () {
-		
+		private static Button CreateButton(Canvas canvas, List<Button> buttons, string buttonText)
+		{
+			var button = Instantiate(Resources.Load<GameObject>("Prefabs/Button")).GetComponent<Button>();
+			buttons.Add(button);
+			button.transform.SetParent(canvas.transform);
+			button.GetComponentInChildren<Text>().text = buttonText;
+			return button;
+		}
+
+		private void CreateBackgroundButton(Canvas canvas, List<Button> buttons)
+		{
+			var backgroundButton = Instantiate(Resources.Load<GameObject>("Prefabs/Background Button")).GetComponent<Button>();
+			buttons.Add(backgroundButton);
+			backgroundButton.transform.SetParent(canvas.transform);
+			backgroundButton.onClick.AddListener(() => DestroyActionMenu(buttons));
+		}
+
+		private static void DestroyActionMenu(IEnumerable<Button> buttons)
+		{
+			buttons
+				.Select(b => b.gameObject)
+				.ToList()
+				.ForEach(Destroy);
 		}
 	}
 }

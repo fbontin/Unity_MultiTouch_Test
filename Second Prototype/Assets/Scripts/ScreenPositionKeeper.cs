0ad50212554b834be5fe1bffacc288006003bba2
Filip Bontin <fbontin@gmail.com>
2017-07-25 14:31:15 +0200

    Drag to add object second commit

---
+++ Second Prototype/Assets/Scripts/ScreenPositionKeeper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class ScreenPositionKeeper : MonoBehaviour
+	{
+
+		public float GoalX;
+		public float GoalY;
+
+		private Camera _cam;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+			_cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			gameObject.transform.position = _cam.ScreenToWorldPoint(new Vector3(GoalX, GoalY, 3));
+		}
+	}
+}
